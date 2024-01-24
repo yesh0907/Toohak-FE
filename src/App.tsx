71c575b341b3700ca6702c59234c10389a806038
Gordon Yee <gordon.n.yee@gmail.com>
2024-01-24 15:21:25 -0800

    started socket connection logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+// import { socket } from './socket';
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">!Toohak Frontend</h1>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">!Toohak Frontend</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
