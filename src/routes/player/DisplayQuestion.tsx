b59623e015d14146d52327c130766e14a4ae4af0
Norton Choy <nomvus848@gmail.com>
2024-02-18 18:00:02 -0800

    removed extra line

--- src/routes/player/DisplayQuestion.tsx
+++ src/routes/player/DisplayQuestion.tsx
@@ -2,8 +2,7 @@
 function DisplayQuestion({ question, answers }: { question: string; answers: [number, string][] }) {
     return (
         <div>
-            <p>Question: </p>
-            <p>question: {question}</p>
+            <p>Question: {question}</p>
                 <ul className="answers">
                     {answers.map(([key, answer]) => (
                         <li key={key}>
