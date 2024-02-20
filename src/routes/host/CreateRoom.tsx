9b019e036aca3fdf3c025405c429b9bc47f40667
Gordon Yee <gordon.n.yee@gmail.com>
2024-02-20 12:50:33 -0800

    fixed deployment error

--- src/routes/host/CreateRoom.tsx
+++ src/routes/host/CreateRoom.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { socket } from "../../socket";
 import Header from "../../components/Header";
 
