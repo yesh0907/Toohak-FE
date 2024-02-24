d3cd347d5d8e4bf4e7384db017d4187ce633de35
Yesh Chandiramani <yesh0907@users.noreply.github.com>
2024-02-24 13:03:05 -0800

    Revert "player can recieve new question event and display question + answers …"
    This reverts commit f01c2ff6ebf90330e3a4c470dbca8a356c51c46a.

--- src/routes/player/JoinRoom.tsx
+++ src/routes/player/JoinRoom.tsx
@@ -5,14 +5,10 @@ import { WS_EVENTS } from "../../socket/events";
 import { observable } from "@legendapp/state";
 
 // global state
-const state$ = observable({
+const State = observable({
   player: {
     id: '',
-  },
-  question: '',
-  answers: [],
-  timer: 30,
-
+  }
 });
 
 // generate a random number from 1-1000 and append to string 'player'
@@ -28,33 +24,20 @@ function JoinRoom() {
       // join websocket room for communication of specific events
       socket.emit(WS_EVENTS.JOIN_ROOM, roomId);
       // set playerId
-      state$.player.id.set(generatePlayerId());
+      State.player.id.set(generatePlayerId());
       // let everyone in the room know there is a new player
       socket.emit(WS_EVENTS.NEW_PLAYER, {
         roomId,
-        playerId: state$.player.id.get(),
+        playerId: State.player.id.get(),
       });
-
-      socket.on("newQuestion", (data) => {
-        state$.question.set(data.question);
-        state$.answers.set(data.answers);
-      })
     }
   }, [roomId]);
 
   return (
-        <>
-            <h1 className="text-3xl font-bold underline">Join room</h1>
-            <p>question: {state$.question.get()}</p>
-            <ul className="answers">
-                    {state$.answers.map((player, idx) => {
-                        return (
-                            <li key={idx}>{player}</li>
-                        );
-                    })}
-                </ul>
-        </>
-    );
+    <>
+      <h1 className="text-3xl font-bold underline">Join room</h1>
+    </>
+  );
 }
 
 export default JoinRoom;
