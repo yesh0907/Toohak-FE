f01c2ff6ebf90330e3a4c470dbca8a356c51c46a
Justin Bui <75401557+justinqbui@users.noreply.github.com>
2024-02-24 12:58:46 -0800

    player can recieve new question event and display question + answers (#8)
    * new question event handler
    
    * typo
    
    * add interface
    
    * Revert "add interface"
    
    This reverts commit 84c6d19294064b3fcba3852992f0c4476d61b046.

--- src/routes/player/JoinRoom.tsx
+++ src/routes/player/JoinRoom.tsx
@@ -5,10 +5,14 @@ import { WS_EVENTS } from "../../socket/events";
 import { observable } from "@legendapp/state";
 
 // global state
-const State = observable({
+const state$ = observable({
   player: {
     id: '',
-  }
+  },
+  question: '',
+  answers: [],
+  timer: 30,
+
 });
 
 // generate a random number from 1-1000 and append to string 'player'
@@ -24,20 +28,33 @@ function JoinRoom() {
       // join websocket room for communication of specific events
       socket.emit(WS_EVENTS.JOIN_ROOM, roomId);
       // set playerId
-      State.player.id.set(generatePlayerId());
+      state$.player.id.set(generatePlayerId());
       // let everyone in the room know there is a new player
       socket.emit(WS_EVENTS.NEW_PLAYER, {
         roomId,
-        playerId: State.player.id.get(),
+        playerId: state$.player.id.get(),
       });
+
+      socket.on("newQuestion", (data) => {
+        state$.question.set(data.question);
+        state$.answers.set(data.answers);
+      })
     }
   }, [roomId]);
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Join room</h1>
-    </>
-  );
+        <>
+            <h1 className="text-3xl font-bold underline">Join room</h1>
+            <p>question: {state$.question.get()}</p>
+            <ul className="answers">
+                    {state$.answers.map((player, idx) => {
+                        return (
+                            <li key={idx}>{player}</li>
+                        );
+                    })}
+                </ul>
+        </>
+    );
 }
 
 export default JoinRoom;
