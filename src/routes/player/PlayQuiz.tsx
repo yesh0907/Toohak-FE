c32b58db3b345fd501676e54eea89ad98b206a51
Yesh Chandiramani <yesh0907@users.noreply.github.com>
2024-02-26 14:21:32 -0500

    implement waiting for next quesiton (#14)

--- src/routes/player/PlayQuiz.tsx
+++ src/routes/player/PlayQuiz.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { socket } from "../../socket";
 import { WS_EVENTS } from "../../socket/events";
 import { useCountdown } from "usehooks-ts";
-import { state$ } from "../../state";
+import { resetQuizState, state$ } from "../../state";
 import ShowAnswer from "../../components/player/ShowAnswer";
 import WaitingForOtherPlayers from "../../components/player/WaitingForOtherPlayers";
 import WaitingForQuestion from "../../components/player/WaitingForQuestion";
@@ -15,12 +15,12 @@ const TIMER_DURATION = 30;
 
 const PlayQuiz = observer(() => {
   // nice hook to simplify the timer logic, default interval: 1000 ms (1 second)
-  const [count, { startCountdown, stopCountdown }] = useCountdown({
+  const [count, { startCountdown, stopCountdown, resetCountdown }] = useCountdown({
     countStart: TIMER_DURATION,
   });
   const showTimer = useObservable(true);
 
-  // send recv question event after component is mounted
+  // send recv question event to first question in quiz after component is mounted
   useEffect(() => {
     // let server know client is ready to answer question
     socket.emit(WS_EVENTS.RECV_QUESTION, state$.roomId.get());
@@ -29,10 +29,12 @@ const PlayQuiz = observer(() => {
   // Start timer logic
   useObserveEffect(() => {
     const handleStartTimer = () => {
-      showTimer.set(true);
-      state$.quiz.displayQuestion.set(true);
-      console.log('starting timer now!');
-      startCountdown();
+      if (!state$.quiz.displayQuestion.get()) {
+        showTimer.set(true);
+        state$.quiz.displayQuestion.set(true);
+        console.log('starting timer now!');
+        startCountdown();
+      }
     }
 
     socket.on(WS_EVENTS.START_TIMER, handleStartTimer);
@@ -42,58 +44,103 @@ const PlayQuiz = observer(() => {
     }
   });
 
-// show answer
-useObserveEffect(() => {
-  const handleShowAnswer = (data: { correctAnswer: string }) => {
-    stopCountdown();
-    showTimer.set(false);
-    console.log(data);
-    const isCorrect = state$.quiz.selectedAnswer.get() === data.correctAnswer;
+  // show answer
+  useObserveEffect(() => {
+    const handleShowAnswer = (data: { correctAnswer: string }) => {
+      const isCorrect = state$.quiz.selectedAnswer.get() === data.correctAnswer;
 
-    state$.quiz.isAnswerCorrect.set(isCorrect);
-    state$.quiz.displayQuestion.set(false);
-    state$.quiz.waitingForOthers.set(false);
-    state$.quiz.answered.set(true);
-    console.log('showing answer');
-  };
+      // stop, reset & hide timer
+      stopCountdown();
+      resetCountdown();
+      showTimer.set(false);
 
-  socket.on(WS_EVENTS.SHOW_ANSWER, handleShowAnswer);
+      // update state
+      state$.quiz.isAnswerCorrect.set(isCorrect);
+      state$.quiz.displayQuestion.set(false);
+      state$.quiz.waitingForOthers.set(false);
+      state$.quiz.answered.set(true);
 
-  return () => {
-    socket.off(WS_EVENTS.SHOW_ANSWER, handleShowAnswer);
-  };
-});
+      // emit waiting for quiz event after 5 seconds of showing answer
+      const waitForQuizTimeout = setTimeout(() => {
+        console.log('waiting for quiz');
+        socket.emit(WS_EVENTS.WAIT_FOR_QUIZ, state$.roomId.get());
+        resetQuizState();
+        clearTimeout(waitForQuizTimeout);
+      }, 5000);
+    };
 
-return (
-  <div className="bg-gray-100 min-h-screen flex flex-col items-center gap-5">
-    <div className="bg-purple-800 min-w-full p-5">
-      <Link to="/" className="text-6xl text-white font-bold">
-        !Toohak
-      </Link>
-    </div>
-    <div className="flex w-full p-2 justify-end">
-      <p className="text-lg">ID: {state$.player.id.get()}</p>
-    </div>
-    <div className="flex flex-col gap-6">
-      {showTimer.get() && <div className="text-2xl text-center">
-        <span className="font-semibold">Time Left:</span>&nbsp;{count} seconds
-      </div>}
-      {state$.quiz.displayQuestion.get() && <DisplayQuestion />}
-      {state$.quiz.waitingForOthers.get() && <WaitingForOtherPlayers />}
-      {
-        state$.quiz.answered.get()
-        && !state$.quiz.waitingForOthers.get()
-        && <ShowAnswer />
+    socket.on(WS_EVENTS.SHOW_ANSWER, handleShowAnswer);
+
+    return () => {
+      socket.off(WS_EVENTS.SHOW_ANSWER, handleShowAnswer);
+    };
+  });
+
+  // get new question
+  useObserveEffect(() => {
+    const handleNewQuestion = (
+      { question, answers }: { question: string; answers: Array<string> }
+    ) => {
+      if (!state$.quiz.displayQuestion.get()) {
+        // set state
+        state$.quiz.question.set(question);
+        state$.quiz.answers.set(answers);
+        // let server know client is ready
+        socket.emit(WS_EVENTS.RECV_QUESTION, state$.roomId.get());
       }
-      {
-        !state$.quiz.displayQuestion.get()
-        && !state$.quiz.waitingForOthers.get()
-        && !state$.quiz.answered.get()
-        && <WaitingForQuestion />
-      }
+    }
+
+    socket.on(WS_EVENTS.NEW_QUESTION, handleNewQuestion);
+
+    return () => {
+      socket.off(WS_EVENTS.NEW_QUESTION);
+    }
+  });
+
+  // quiz is completed
+  useObserveEffect(() => {
+    // TODO
+    const handleQuizCompleted = () => {
+      console.log('yay we are done with the quiz!');
+    }
+
+    socket.on(WS_EVENTS.QUIZ_COMPLETED, handleQuizCompleted);
+
+    return () => {
+      socket.off(WS_EVENTS.QUIZ_COMPLETED);
+    }
+  });
+
+  return (
+    <div className="bg-gray-100 min-h-screen flex flex-col items-center gap-5">
+      <div className="bg-purple-800 min-w-full p-5">
+        <Link to="/" className="text-6xl text-white font-bold">
+          !Toohak
+        </Link>
+      </div>
+      <div className="flex w-full p-2 justify-end">
+        <p className="text-lg">ID: {state$.player.id.get()}</p>
+      </div>
+      <div className="flex flex-col gap-6">
+        {showTimer.get() && <div className="text-2xl text-center">
+          <span className="font-semibold">Time Left:</span>&nbsp;{count} seconds
+        </div>}
+        {state$.quiz.displayQuestion.get() && <DisplayQuestion />}
+        {state$.quiz.waitingForOthers.get() && <WaitingForOtherPlayers />}
+        {
+          state$.quiz.answered.get()
+          && !state$.quiz.waitingForOthers.get()
+          && <ShowAnswer />
+        }
+        {
+          !state$.quiz.displayQuestion.get()
+          && !state$.quiz.waitingForOthers.get()
+          && !state$.quiz.answered.get()
+          && <WaitingForQuestion />
+        }
+      </div>
     </div>
-  </div>
-)
+  )
 });
 
 export default PlayQuiz;
