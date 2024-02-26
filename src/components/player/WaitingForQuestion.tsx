ac767f1474c2c01bb6941fb3d11861867b5698ec
Yesh Chandiramani <yeshc0907@gmail.com>
2024-02-25 22:05:48 -0800

    remove player waiting for quiz route and fix naming typo of player WaitingForQuestion component

--- src/components/player/WaitingForQuestion.tsx
+++ src/components/player/WaitingForQuestion.tsx
@@ -1,4 +1,4 @@
-export default function WaitingFoQuestion() {
+export default function WaitingForQuestion() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen">
             <h1 className="text-3xl text-black mb-3">Waiting for question...</h1>
