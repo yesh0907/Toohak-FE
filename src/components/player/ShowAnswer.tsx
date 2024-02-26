8a9f458843a86fa31c7d7656adf7f750e50dde65
Gordon Yee <gordon.n.yee@gmail.com>
2024-02-26 01:03:29 -0800

    Componentized header and added visuals for ShowAnswer

--- src/components/player/ShowAnswer.tsx
+++ src/components/player/ShowAnswer.tsx
@@ -1,16 +1,19 @@
 import { state$ } from "../../state";
 
-function ShowAnswer() {    
-    
-    // wait 5 sec
-    // emit WS(waitingforquiz)
-    // navigate to WaitingForNextQuestion
-
-    return (
-        <div>
-            {state$.quiz.isAnswerCorrect.get() ? 'yay you got it right' : 'boo wrong'}
+function ShowAnswer() {
+  return (
+    <>
+      {state$.quiz.isAnswerCorrect.get() ? (
+        <div className="w-auto h-auto flex flex-col justify-center items-center p-12 rounded-3xl bg-green-600">
+          <div className="text-8xl font-thin text-gray-100">Correct!</div>
         </div>
-    );
+      ) : (
+        <div className="w-auto h-auto flex flex-col justify-center items-center p-12 rounded-3xl bg-red-700">
+          <div className="text-8xl font-thin text-gray-100">Incorrect.</div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default ShowAnswer;
--- src/routes/player/JoinRoom.tsx
+++ src/routes/player/JoinRoom.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { socket } from "../../socket";
 import { WS_EVENTS } from "../../socket/events";
 import { state$ } from "../../state";
+import Header from "../../components/Header";
 
 // generate a random number from 1-1000 and append to string 'player'
 const generatePlayerId = () => `player${Math.floor(Math.random() * 1000) + 1}`;
@@ -26,30 +27,30 @@ function JoinRoom() {
         playerId: state$.player.id.get(),
       });
 
-      const handleNewQuestion = (
-        { question, answers }: { question: string; answers: Array<string> }
-      ) => {
+      const handleNewQuestion = ({
+        question,
+        answers,
+      }: {
+        question: string;
+        answers: Array<string>;
+      }) => {
         // set state
         state$.quiz.question.set(question);
         state$.quiz.answers.set(answers);
         // navigate to play quiz view
         navigate(`/quiz/${roomId}`);
-      }
+      };
       socket.on(WS_EVENTS.NEW_QUESTION, handleNewQuestion);
 
       return () => {
         socket.off(WS_EVENTS.NEW_QUESTION);
-      }
+      };
     }
   }, [roomId, navigate]);
 
   return (
     <div className="bg-gray-100 min-h-screen flex flex-col items-center gap-5">
-      <div className="bg-purple-800 min-w-full p-5">
-        <Link to="/" className="text-6xl text-white font-bold">
-          !Toohak
-        </Link>
-      </div>
+      <Header />
       <div className="flex flex-row-reverse w-full gap-3 p-2">
         <p className="text-lg">ID: {state$.player.id.get()}</p>
         <h1 className="flex-1 text-center text-3xl font-bold">
